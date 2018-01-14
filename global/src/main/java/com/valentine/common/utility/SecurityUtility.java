aa6b9480d979e55a6eebac164a5690bef0538f71
valentine <valentineezugu@yahoo.com>
2018-01-14 23:16:08 +0300

    test

--- global/src/main/java/com/valentine/common/utility/SecurityUtility.java
+++ global/src/main/java/com/valentine/common/utility/SecurityUtility.java
@@ -3,7 +3,6 @@ package com.valentine.common.utility;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
-
 import java.security.SecureRandom;
 import java.util.Random;
 
