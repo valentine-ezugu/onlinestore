38337d4d40ce3d3618910a470491d2e7d47f08ad
Siarhei Piashko <peshko.sergey@gmail.com>
2018-01-15 15:58:42 +0300

    big refactoring

---
+++ bookstore/rest/src/test/java/dozer/DozerTest.java
@@ -0,0 +1,30 @@
+package dozer;
+
+import com.valentine.bookstore.config.MappingConfig;
+import com.valentine.domain.UserShipping;
+import com.valentine.dto.user.UserForShippingLite;
+import org.dozer.Mapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MappingConfig.class})
+public class DozerTest {
+
+    @Autowired
+    private Mapper mapper;
+
+    @Test
+    public void dozerTest() throws Exception {
+
+        UserShipping userShipping = new UserShipping();
+        userShipping.setUserShippingName("Chiko");
+        UserForShippingLite userForShippingLite = mapper.map(userShipping, UserForShippingLite.class, "userShippingLiteId");
+        Assert.assertEquals(userShipping.getUserShippingName(), userForShippingLite.getUserShippingName());
+    }
+
+}
